03372c48b6e2baa32da04625cb857d42f3aa9756
VINGROUP\hant168 <hanguyen2411hd@gmail.com>
2021-05-12 22:58:16 +0700

    commit

--- BTVN_02/NgayThangNam.java
+++ BTVN_02/NgayThangNam.java
@@ -4,32 +4,35 @@ public class NgayThangNam {
     public int thang;
     public int nam;
 
-    public void tinhNgayThangNam(){
+    public void tinhNgayThangNam() {
 
-       switch(thang){
-           case 1,3,5,7,8,10,12: 
-           System.out.println("Thang "+ thang+ "co 31 ngay");
-           break;
-           case 4,6,9,11:
-           System.out.println("Thang "+ thang+ "co 30 ngay");
-           break;
-           case 2:
-                //int nam;
-                Scanner m= new Scanner(System.in);
+        switch (thang) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                System.out.println("Thang " + thang + "co 31 ngay");
+                break;
+            case 4, 6, 9, 11:
+                System.out.println("Thang " + thang + "co 30 ngay");
+                break;
+            case 2:
+                Scanner m = new Scanner(System.in);
                 System.out.print("Moi nhap nam:");
                 nam = m.nextInt();
-                if((nam%4==0) && (nam%100!=0)||(nam%400==0)){
-                    System.out.println("thang"+thang+" co 29 ngay");
+                if ((nam % 4 == 0)) {
+                    if (nam % 100 == 0) {
+                        if (nam % 400 == 0) {
+                            System.out.println("Tháng" + thang + "có 29 ngày");
+                        }
+                    } else {
+                        System.out.println("Tháng" + thang + "có 28 ngày");
                     }
-                 else System.out.println("thang"+thang+" co 28 ngay");
-                 break;
+                }
+                break;
+            default: {
+                System.out.println("Nhap thang khong hop le");
+            }
 
-            default:{System.out.println("Nhap thang khong hop le");
+        }
 
-           }
+    }
 
-       }
-
-   }
-   
 }
